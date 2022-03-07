c0c706a2faa9881a30de7ee00f3959bcbc9ded5a
Shashank Gupta <gupta.shashank94@gmail.com>
2022-03-07 21:50:18 +0530

    Added Third File

--- Sum.java
+++ Sum.java
@@ -1,7 +1,9 @@
 public class Sum {
     public static void main(String[] args)
+    {
         int a=5;
         int b=6;
 
-        system.out.println("The Sum is :" +  (a+b));
+        System.out.println("The Sum is :" +  (a+b));
+    }
 }
